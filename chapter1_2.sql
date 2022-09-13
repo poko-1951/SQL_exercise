00662d7d0e2c08434b7720e596651548a6372942
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-09-13 21:16:04 +0900

    [Add]1章

--- chapter1_2.sql
+++ chapter1_2.sql
@@ -1,2 +1,19 @@
--- list 1_1
-select expense from housekeepingdata;
+-- -- list 1_1
+-- SELECT expense FROM housekeepingdata;
+
+-- -- list1_3
+SELECT * FROM housekeepingdata;
+
+-- -- list1_4
+-- SELECT date_data, item, expense
+-- FROM housekeepingdata
+-- WHERE expense > 3000
+
+-- -- list1-6
+-- UPDATE housekeepingdata
+-- SET expense = 90000
+-- WHERE date_data = "2018-02-25";
+
+-- list1-7
+-- DELETE FROM housekeepingdata
+-- WHERE date_data = "2018-02-25";
