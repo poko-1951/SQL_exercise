cb074eb29890f9eaa6793d0f34b89a30535841f6
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-09-21 22:01:06 +0900

    [Add]1章練習問題

--- chapter1.sql
+++ chapter1.sql
@@ -2,7 +2,7 @@
 -- SELECT expense FROM housekeepingdata;
 
 -- -- list1_3
-SELECT * FROM housekeepingdata;
+-- SELECT * FROM housekeepingdata;
 
 -- -- list1_4
 -- SELECT date_data, item, expense
@@ -17,3 +17,17 @@ SELECT * FROM housekeepingdata;
 -- list1-7
 -- DELETE FROM housekeepingdata
 -- WHERE date_data = "2018-02-25";
+
+-- test1-3-1
+-- SELECT *
+-- FROM housekeepingdata
+-- WHERE income = 50000
+
+-- test1-3-2
+-- DELETE FROM housekeepingdata
+-- WHERE expense > 4000
+
+-- test1-3-3
+-- UPDATE housekeepingdata
+-- SET memo = "カフェラテを購入"
+-- WHERE date_data = "2018-02-03"
