2834ce28d36949b5324e9cf8da8fd0331c380d2e
poko-1951 <103019339+poko-1951@users.noreply.github.com>
2022-09-24 23:59:59 +0900

    [Add]ドリルB

--- exercise_B.sql
+++ exercise_B.sql
@@ -36,3 +36,27 @@ GROUP BY 商品コード
 HAVING SUM( 数量 ) >ALL(SELECT AVG( 数量 ) 
                         FROM 注文 
                         GROUP BY 商品コード )
+
+-- 59
+SELECT A.数量合計 AS 割引による販売数,
+       TRUNC (A.割引料合計 / A.数量合計, 0) AS 平均割引額
+FROM (SELECT SUM( 数量 ) AS 数量合計, 
+      SUM( クーポン割引料 ) AS 割引料合計 
+      FROM 注文
+      WHERE 商品コード = 'W0746' 
+      AND クーポン割引料 IS NOT NULL) AS A
+
+-- 60
+INSERT INTO 注文 
+SELECT 注文日 , 注文番号 , MAX( 注文枝番 ) + 1, 'S1003', 1, NULL
+FROM 注文 
+WHERE 注文日 = '2018-03-21' 
+AND 注文番号 = '201803210080' 
+GROUP BY 注文日, 注文番号;
+
+INSERT INTO 注文 
+SELECT 注文日, 注文番号, MAX( 注文枝番 ) + 1, 'S1003', 1, NULL
+FROM 注文 
+WHERE 注文日 = '2018-03-22' 
+AND 注文番号 = '201803220901' 
+GROUP BY 注文日, 注文番号;
